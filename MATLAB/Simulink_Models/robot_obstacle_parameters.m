7b45e4b2de094a8df692ad7ad9c91bc4711e8570
Haider Matin <haider.matin@outlook.com>
2024-01-29 16:37:56 +0100

    Add files via upload

---
+++ MATLAB/Simulink_Models/robot_obstacle_parameters.m
@@ -0,0 +1,10 @@
+close all
+clearvars
+clc
+
+%=========================================================================
+% WS2223_DSS_06_robot_obstacle_example_parameters
+%=========================================================================
+m = 3.0; %mass in kg
+k_drag = 30; %rolling and air resistance in Ns^2/m^2
+r = 0.15; %radius of robot in m
